9ed8880b040c8012ff2e0443d97046a866da4a22
Hendri Alqori <teamhendri18@gmail.com>
2025-07-05 13:25:32 +0700

    feat: create dotenv import file

---
+++ server/src/constants/dotenv.ts
@@ -0,0 +1,7 @@
+export const DATABASE_URI = process.env.DATABASE_URI;
+export const CLIENT_URL = process.env.CLIENT_ID;
+export const PORT = process.env.PORT;
+export const JWT_ACCESS_KEY = process.env.JWT_ACCESS_KEY;
+
+export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+export const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
