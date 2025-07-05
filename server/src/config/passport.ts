95996500e4e491c086cd6bb8c742823e692e145a
Hendri Alqori <teamhendri18@gmail.com>
2025-07-05 13:25:14 +0700

    feat: setup auth with passport.js

---
+++ server/src/config/passport.ts
@@ -0,0 +1,35 @@
+import passport from "passport";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from "@/constants/dotenv";
+import * as authRepo from "@/repositories/AuthRepository";
+import { UserSelect } from "@/db/schemas/User";
+
+const googleStrategy = new GoogleStrategy(
+  {
+    clientID: String(GOOGLE_CLIENT_ID),
+    clientSecret: String(GOOGLE_CLIENT_SECRET),
+    callbackURL: "/api/auth/google/callback",
+  },
+  async (_accessToken, _refreshToken, profile, done) => {
+    // collect google crendential data
+    const googleCredential = {
+      display_name: profile.displayName,
+      email: String(profile.emails?.[0].value),
+      picture: String(profile.photos?.[0].value),
+      provider_id: profile.id,
+    } as UserSelect;
+
+    // check if there is user with provider id equal to google credential id
+    const user = await authRepo.show(profile.id);
+
+    // if not exits, create new user with these credential
+    if (!user) {
+      await authRepo.create(googleCredential);
+      return done(null, googleCredential);
+    }
+
+    return done(null, user);
+  },
+);
+
+passport.use(googleStrategy);
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,13 @@
 import "./config/aliases";
+import "./config/passport";
 import "dotenv/config";
 import express from "express";
+import cookierParser from "cookie-parser";
+import cors from "cors";
+import passport from "passport";
 import routes from "./routes";
 import { errorMiddleware } from "./middlewares/ErrorMiddleware";
+import { CLIENT_URL, PORT } from "./constants/dotenv";
 
 const app = express();
 
@@ -10,11 +15,22 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/api/v1", routes);
+app.use(cookierParser());
+
+app.use(passport.initialize());
+
+app.use(
+  cors({
+    origin: CLIENT_URL,
+    credentials: true,
+  }),
+);
+
+app.use("/api", routes);
 
 app.use(errorMiddleware);
 
-const server = app.listen(8080, "0.0.0.0", (error) => {
+const server = app.listen(Number(PORT), "0.0.0.0", (error) => {
   if (error) throw error;
   console.log(`Server listening on ${JSON.stringify(server.address())}`);
 });
