628c4b9b3b1c37ddfe694796f64b506db0875043
Hendri Alqori <teamhendri18@gmail.com>
2025-07-07 13:17:34 +0700

    feat: create user schema from user table

--- server/src/db/schemas/User.ts
+++ server/src/db/schemas/User.ts
@@ -1,17 +1,21 @@
 import { genuuid } from "@/utils/uuid";
-import { mysqlTable, text, timestamp, varchar } from "drizzle-orm/mysql-core";
+import { mysqlEnum, mysqlTable, text, timestamp, varchar } from "drizzle-orm/mysql-core";
+import { createInsertSchema } from "drizzle-zod";
 
 export const users = mysqlTable("users", {
-  id: varchar("id", { length: 8 })
-    .primaryKey()
-    .$defaultFn(() => genuuid()),
-  email: varchar("email", { length: 50 }).notNull(),
-  display_name: varchar("display_name", { length: 255 }).notNull(),
-  picture: text("picture").notNull(),
-  provider_id: text("provider_id").notNull(),
-  created_at: timestamp("created_at").defaultNow(),
-  updated_at: timestamp("updated_at"),
+    id: varchar("id", { length: 8 })
+        .primaryKey()
+        .$defaultFn(() => genuuid()),
+    email: varchar("email", { length: 50 }).notNull(),
+    display_name: varchar("display_name", { length: 255 }).notNull(),
+    picture: text("picture").notNull(),
+    provider_id: text("provider_id").notNull(),
+    role: mysqlEnum("role", ["ADMIN", "BUYER"]).default("BUYER"),
+    created_at: timestamp("created_at").defaultNow(),
+    updated_at: timestamp("updated_at"),
 });
+
+export const createUserSchema = createInsertSchema(users);
 
 export type UserSelect = typeof users.$inferSelect;
 
