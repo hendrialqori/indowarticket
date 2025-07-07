3e88d8a1efe85bda23a0878813b64a7aa6a12855
Hendri Alqori <teamhendri18@gmail.com>
2025-07-07 13:11:55 +0700

    feat: add event and ticket route

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,8 +1,12 @@
 import express from "express";
 import authRoute from "@/routes/AuthRoute";
+import eventRoute from "@/routes/EventRoute";
+import ticketRoute from '@/routes/TicketRoute'
 
 const router = express.Router();
 
 router.use("/auth", authRoute);
+router.use("/event", eventRoute);
+router.use("/ticket", ticketRoute)
 
 export default router;
