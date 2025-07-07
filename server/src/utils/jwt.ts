0c332d8716281de32aca328ee7abfdfe40f36ba3
Hendri Alqori <teamhendri18@gmail.com>
2025-07-07 13:10:05 +0700

    refactor: increase time expires jwt token

--- server/src/utils/jwt.ts
+++ server/src/utils/jwt.ts
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 import { JWT_ACCESS_KEY } from "@/constants/dotenv";
 
 export const generateToken = (user: Express.User): string => {
-  return jwt.sign(user, String(JWT_ACCESS_KEY), {
-    expiresIn: "15m",
-  });
+    return jwt.sign(user, String(JWT_ACCESS_KEY), {
+        expiresIn: "45m",
+    });
 };
