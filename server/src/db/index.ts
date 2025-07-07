018cff5c471c7dfa19516c8af8651da558f37769
Hendri Alqori <teamhendri18@gmail.com>
2025-07-07 13:18:06 +0700

    refactor: change connect timeout database to 5s

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -3,8 +3,8 @@ import { drizzle } from "drizzle-orm/mysql2";
 import mysql from "mysql2/promise";
 
 const poolConnection = mysql.createPool({
-  uri: process.env.DATABASE_URI,
-  connectTimeout: 50000,
+    uri: process.env.DATABASE_URI,
+    connectTimeout: 50000,
 });
 const db = drizzle({ client: poolConnection });
 
