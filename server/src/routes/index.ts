5533bd4e301a9fd5255490f2f0b73839a58679d8
Hendri Alqori <teamhendri18@gmail.com>
2025-07-09 12:57:10 +0700

    feat: add transation route

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -2,11 +2,13 @@ import express from "express";
 import authRoute from "@/routes/AuthRoute";
 import eventRoute from "@/routes/EventRoute";
 import ticketRoute from '@/routes/TicketRoute'
+import transactionRoute from '@/routes/TransactionRoute'
 
 const router = express.Router();
 
 router.use("/auth", authRoute);
 router.use("/event", eventRoute);
 router.use("/ticket", ticketRoute)
+router.use("/transaction", transactionRoute)
 
 export default router;
