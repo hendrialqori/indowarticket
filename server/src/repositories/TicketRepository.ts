a29e6dd15da8a05bde8319d4e356b2cceee30566
Hendri Alqori <teamhendri18@gmail.com>
2025-07-09 12:47:24 +0700

    chore: remove show, create, update and remove transations repository

--- server/src/repositories/TicketRepository.ts
+++ server/src/repositories/TicketRepository.ts
@@ -1,58 +1,41 @@
 import { db } from "@/db";
-import { TicketInsert, tickets as ticketsTable } from "@/db/schemas/Ticket";
-import { users as usersTable } from "@/db/schemas/User";
-import { events as eventsTable } from "@/db/schemas/Event";
-import { eq, sql } from "drizzle-orm";
+import { tickets } from "@/db/schemas/Ticket";
+import { eq } from "drizzle-orm";
+import { transactions } from "@/db/schemas/Transaction";
+import { events } from "@/db/schemas/Event";
+import { users } from "@/db/schemas/User";
 
 export async function index() {
-    const mappedRow = {
-        id: ticketsTable.id,
-        code: ticketsTable.code,
+    const rows = {
+        id: tickets.id,
+        status: tickets.status,
         user: {
-            id: usersTable.id,
-            email: usersTable.email,
-            display_name: usersTable.display_name,
-            provider_id: usersTable.provider_id
+            id: users.id,
+            email: users.email,
+            display_name: users.display_name
         },
         event: {
-            id: eventsTable.id,
-            name: eventsTable.name,
-            start_time: eventsTable.start_time
+            name: events.name,
+            description: events.description,
+            start_time: events.start_time,
+            status: events.status,
+            ticket_price: events.ticket_price
         },
-        purchase_time: ticketsTable.purchase_time,
-        created_at: ticketsTable.created_at
+        transaction: {
+            purchase_time: transactions.purchase_time,
+            status: transactions.status
+        },
+        created_at: tickets.created_at,
+        updated_at: tickets.updated_at
     }
 
-    const tickets =
-        await db.select(mappedRow).from(ticketsTable)
-            .innerJoin(usersTable, eq(usersTable.id, ticketsTable.user_id))
-            .innerJoin(eventsTable, eq(eventsTable.id, ticketsTable.event_id))
+    const results =
+        await db.select(rows).from(tickets)
+            .innerJoin(transactions, eq(transactions.id, tickets.transaction_id))
+            .innerJoin(users, eq(users.id, transactions.user_id))
+            .innerJoin(events, eq(events.id, transactions.event_id))
+            .where(eq(transactions.id, tickets.transaction_id))
 
-    return tickets
+    return results
 }
 
-export async function show(id: string) {
-    const [ticket] = await db.select().from(ticketsTable).where(eq(ticketsTable.id, id))
-    return ticket
-}
-
-export async function create(payload: TicketInsert) {
-    await db.transaction(async (tx) => {
-        try {
-            await db.insert(ticketsTable).values(payload)
-            await db.update(eventsTable)
-                .set({ ticket_available: sql`${eventsTable.ticket_available} - 1` })
-                .where(eq(eventsTable.id, String(payload.event_id)))
-        } catch (error) {
-            tx.rollback()
-        }
-    })
-}
-
-export async function update(id: string, payload: TicketInsert) {
-    await db.update(ticketsTable).set(payload).where(eq(ticketsTable.id, id))
-}
-
-export async function remove(id: string) {
-    await db.delete(ticketsTable).where(eq(ticketsTable.id, id))
-}
