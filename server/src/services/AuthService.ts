cfc44dfb8dd6051d99cf73519dbae77ccaf77a44
Hendri Alqori <teamhendri18@gmail.com>
2025-07-09 12:44:56 +0700

    feat: create auth transaction and ticket service

--- server/src/services/AuthService.ts
+++ server/src/services/AuthService.ts
@@ -2,6 +2,16 @@ import * as authRepository from "@/repositories/AuthRepository";
 import { type UserInsert, createUserSchema } from "@/db/schemas/User";
 import { ValidationError } from "@/utils/errors/ValidationError";
 
+export async function transaction(id: string) {
+    const result = await authRepository.transaction(id)
+    return result
+}
+
+export async function ticket(id: string) {
+    const result = await authRepository.ticket(id)
+    return result
+}
+
 export async function create(payload: UserInsert) {
     const result = createUserSchema.safeParse(payload);
 
