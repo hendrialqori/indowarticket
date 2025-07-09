77711a372a5c99cdce7a327ceb810d0f10983782
Hendri Alqori <teamhendri18@gmail.com>
2025-07-09 12:58:46 +0700

    feat: add XENDIT_SECRET_KEY

--- server/src/constants/dotenv.ts
+++ server/src/constants/dotenv.ts
@@ -5,3 +5,5 @@ export const JWT_ACCESS_KEY = process.env.JWT_ACCESS_KEY;
 
 export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 export const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+
+export const XENDIT_SECRET_KEY = process.env.XENDIT_SECRET_KEY
