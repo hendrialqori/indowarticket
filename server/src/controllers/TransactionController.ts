8261b0fa7ba516760fc52ad5b7b9d6b965865905
Hendri Alqori <teamhendri18@gmail.com>
2025-07-09 12:50:17 +0700

    chore: remove paid and failed controller function of transaction

--- server/src/controllers/TransactionController.ts
+++ server/src/controllers/TransactionController.ts
@@ -1,45 +1,27 @@
+import { UserSelect } from '@/db/schemas/User';
 import * as transactionService from '@/services/TransactionService'
+import { TransactionIndexQuery } from '@/types/Transaction';
 import { mockApiSuccess } from '@/utils/mockResponse';
 import { Request, Response } from 'express';
 import status from 'http-status';
 
-export async function indexByTicketId(req: Request, res: Response) {
-    const ticketId = req.params.id
-    const tickets = await transactionService.indexByTicketId(ticketId)
-
+export async function index(req: Request, res: Response) {
+    const query = req.query as TransactionIndexQuery
+    const transactions = await transactionService.index(query)
     mockApiSuccess(res, {
         statusCode: status.OK,
         message: 'ok',
-        data: tickets
+        data: transactions
     })
 }
 
 export async function create(req: Request, res: Response) {
-    const requestBody = req.body
+    const userId = (req.user as UserSelect).id
+    const requestBody = { event_id: req.body.event_id, user_id: userId }
     await transactionService.create(requestBody)
     mockApiSuccess(res, {
         statusCode: status.OK,
         message: 'created',
         data: requestBody
-    })
-}
-
-export async function paid(req: Request, res: Response) {
-    const id = req.params.id
-    await transactionService.paid(id)
-    mockApiSuccess(res, {
-        statusCode: status.OK,
-        message: 'transaction paid',
-        data: {}
-    })
-}
-
-export async function failed(req: Request, res: Response) {
-    const id = req.params.id
-    await transactionService.failed(id)
-    mockApiSuccess(res, {
-        statusCode: status.OK,
-        message: 'transaction failed',
-        data: {}
     })
 }
