74673ab334dfa74b075b8523fc5992e83a08db37
Hendri Alqori <teamhendri18@gmail.com>
2025-07-09 12:57:57 +0700

    chore: remove paid and failed function

--- server/src/services/TransactionService.ts
+++ server/src/services/TransactionService.ts
@@ -1,28 +1,27 @@
 import { type TransactionInsert, createTransactionSchema } from '@/db/schemas/Transaction';
 import * as transactionRepository from '@/repositories/TransactionRepository'
+import * as userRepository from '@/repositories/AuthRepository'
+import * as eventRepository from '@/repositories/EventRepository'
+import * as xenditService from '@/services/XenditService'
+import { TransactionIndexQuery } from '@/types/Transaction';
 import { ValidationError } from '@/utils/errors/ValidationError';
-import { validateId } from '@/utils/validate';
 
-export async function indexByTicketId(ticket_id: string) {
-    const validTicketId = validateId(ticket_id)
-    const result = await transactionRepository.indexByTicketId(validTicketId)
+export async function index(query: TransactionIndexQuery) {
+    const result = await transactionRepository.index(query)
     return result
 }
 
 export async function create(payload: TransactionInsert) {
+    const user = await userRepository.show(String(payload.user_id))
+    const event = await eventRepository.show(String(payload.event_id))
+
+    // create invoice 
+    // const invoice = await xenditService.create({ user, event })
+
+    // payload.invoice_url = invoice.invoiceUrl
     const result = createTransactionSchema.safeParse(payload)
     if (!result.success) {
         throw new ValidationError(result.error, "Error occured when parse create transaction");
     }
     await transactionRepository.create(result.data)
-}
-
-export async function paid(id: string) {
-    const validId = validateId(id)
-    await transactionRepository.paid(validId)
-}
-
-export async function failed(id: string) {
-    const validId = validateId(id)
-    await transactionRepository.failed(validId)
 }
