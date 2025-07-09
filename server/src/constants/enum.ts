4591df8473094fc3071f8e8a6b7fe4a6de3f88bf
Hendri Alqori <teamhendri18@gmail.com>
2025-07-09 12:56:30 +0700

    feat:  add status and payment enum

--- server/src/constants/enum.ts
+++ server/src/constants/enum.ts
@@ -2,3 +2,14 @@ export const ROLE = {
     ADMIN: 'ADMIN',
     BUYER: "BUYER"
 } as const
+
+export const STATUS = {
+    VALID: "VALID",
+    USED: "USED"
+} as const
+
+export const PAYMENT = {
+    PENDING: "PENDING",
+    PAID: "PAID",
+    FAILED: "FAILED"
+} as const
