babba7c416640a0cb15ae65013f1653ebba12087
Hendri Alqori <teamhendri18@gmail.com>
2025-07-10 09:37:44 +0700

    fix: add primary key in conversation table

--- server/src/db/schemas/Conversation.ts
+++ server/src/db/schemas/Conversation.ts
@@ -4,7 +4,7 @@ import { users } from "./User";
 import { events } from "./Event";
 
 export const conversations = mysqlTable('conversations', {
-    id: varchar('id', { length: 8 }).$defaultFn(() => genuuid()),
+    id: varchar('id', { length: 8 }).primaryKey().$defaultFn(() => genuuid()),
     admin_id: varchar('admin_id', { length: 8 }).references(() => users.id, { onDelete: "set null" }),
     buyer_id: varchar('buyer_id', { length: 8 }).references(() => users.id, { onDelete: "set null" }),
     event_id: varchar("event_id", { length: 8 }).references(() => events.id, { onDelete: "set null" }),
