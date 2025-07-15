9799c5e9c5b8151d614677eeb97facc074200d56
Hendri Alqori <teamhendri18@gmail.com>
2025-07-15 20:16:36 +0700

    feat: create auth layout

---
+++ client/layouts/auth.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+    <div class="flex bg-[#F6F8FA]">
+        <AppSidebar />
+        <main class="flex-1">
+            <AppContentHeader />
+            <slot></slot>
+        </main>
+    </div>
+</template>
